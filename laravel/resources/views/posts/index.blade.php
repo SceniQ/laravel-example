5d464c724df1fa69eacafb22c0f4ce01fef1c2a4
SceniQ <tassagaai19@gmail.com>
2024-08-13 20:08:42 +0200

    code clean up

--- laravel/resources/views/posts/index.blade.php
+++ laravel/resources/views/posts/index.blade.php
@@ -4,21 +4,20 @@
 
         <div class="grid grid-cols-2 gap-6">
             @foreach ($posts as $post)
-            <div class="card">
-                {{-- Title --}}
-                <h1 class="font-bold text-xl"> {{$post->title}}</h1>
-                {{-- Author & Date --}}
-                <div class="text-xs font-light mb-4">
-                    <span>Posted {{$post->created_at->diffForHumans()}} by</span>
-                    <a href="" class="text-blue-500 font-medium">username</a>
+                <div class="card">
+                    {{-- Title --}}
+                    <h1 class="font-bold text-xl"> {{$post->title}}</h1>
+                    {{-- Author & Date --}}
+                    <div class="text-xs font-light mb-4">
+                        <span>Posted {{$post->created_at->diffForHumans()}} by</span>
+                        <a href="" class="text-blue-500 font-medium">username</a>
+                    </div>
+                    {{-- Body --}}
+                    <div class="text-sm">
+                        <p>{{ Str::words($post->body,15)}}</p>
+                    </div>
                 </div>
-                {{-- Body --}}
-                <div class="text-sm">
-                    <p>{{ Str::words($post->body,15)}}</p>
-                </div>
-
-            </div>
-        @endforeach
+            @endforeach
         </div>
     @endauth
     @guest
